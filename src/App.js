195489663e68a7be631228b0c43314f78c25bba2
nayaknishant656 <nayaknishant656@gmail.com>
2026-07-31 15:56:18 +0530

    Added: Header Component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,16 @@
 import { PipelineToolbar } from './toolbar';
 import { PipelineUI } from './ui';
 import { SubmitButton } from './submit';
+import Header from './FronComponent/Header.jsx'
 
 function App() {
   return (
     <div>
+      <Header />
       <PipelineToolbar />
       <PipelineUI />
       <SubmitButton />
     </div>
-     
-     
-
-
-
 
   );
 }
