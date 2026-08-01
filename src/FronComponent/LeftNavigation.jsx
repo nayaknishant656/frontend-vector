3811944b514e5da3574443fe1b3620e0f12daea7
nayaknishant656 <nayaknishant656@gmail.com>
2026-08-01 14:55:47 +0530

    developed: Developed the CSS For tool components along with Changed and Developed CSS for the LeftNavigation Components

---
+++ src/FronComponent/LeftNavigation.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../css/LeftNavigation.css'
+export default function LeftNavigation() {
+    return (
+        <div className='grandparent-leftNavigation'>
+            <div className='parent-leftNavigation'>
+
+            </div>
+        </div>
+    )
+}
