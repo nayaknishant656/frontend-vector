cb266ad9b0dd3883574dd1fbf127415d763b2020
nayaknishant656 <nayaknishant656@gmail.com>
2026-08-01 14:54:32 +0530

    developed: toolbar component with additional CSS in the Frontend Components

--- src/toolbar.js
+++ src/toolbar.js
@@ -1,10 +1,9 @@
 // toolbar.js
 // src/components/PipelineToolbar.jsx
-
 import React from 'react';
 import { DraggableNode } from './draggableNode';
 import NodeRegistry from '../src/components/nodes/NodeRegistry';
-
+import '../src/css/toolbutton.css'
 export const PipelineToolbar = () => {
 
     // Get all node types from NodeRegistry
@@ -18,21 +17,43 @@ export const PipelineToolbar = () => {
     const nodeTypes = getNodeTypes();
 
     return (
-        <div style={{ padding: '10px' }}>
-            <div style={{
-                marginTop: '20px',
-                display: 'flex',
-                flexWrap: 'wrap',
-                gap: '10px'
-            }}>
-                {nodeTypes.map(({ type, config }) => (
-                    <DraggableNode
-                        key={type}
-                        type={type}
-                        label={config.title || type}
-                    />
-                ))}
+
+        <>
+            <div className='sidebar_grandparent'>
+                <div className='sidebar-parent'>
+                    <div className='tool-button-parent'>
+                        <div className='tool-button-child'>
+                            {nodeTypes.map(({ type, config }) => (
+                                <DraggableNode
+                                    key={type}
+                                    type={type}
+                                    label={config.title || type}
+                                />
+                            ))}
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
+
+
+        </>
+        // <div style={{ padding: '10px' }}>
+        //     <div style={{
+        //         marginTop: '20px',
+        //         display: 'flex',
+        //         flexWrap: 'wrap',
+        //         gap: '10px',
+        //         border: 'solid red 1px'
+
+        //     }}>
+        //         {nodeTypes.map(({ type, config }) => (
+        //             <DraggableNode
+        //                 key={type}
+        //                 type={type}
+        //                 label={config.title || type}
+        //             />
+        // //         ))}
+        //     </div>
+        // </div>
     );
 };
